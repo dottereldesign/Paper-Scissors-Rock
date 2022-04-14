b40d0d89176e30c2209d2cabb74faaeec07dfe61
Jamie Wilson <howemanning@gmail.com>
2022-04-14 21:16:53 +1200

    Created function which asks the user for their input and returns the result.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,17 @@
-// Pseudocode:
 // Paper > Rock > Scissors > Paper
 
-// Pseudocode:
-// Create a function called computerPlay that will randomly return Paper, Scissors, or Rock.
+// Create a blank HTML document with a script tag (Hint: it is best practice to link an external .js file). This game is going to be played completely from the console, so don’t worry about putting anything else in there.
 
 function computerPlay() {
   let computerHand = ["Paper", "Scissors", "Rock"];
   return computerHand[Math.floor(Math.random() * computerHand.length)];
 }
-
 // console.log(computerPlay());
 
-// Pseudocode:
-// Create a function called playRound with two paremeters; playerSelection and computerSelection, that will return a string to declare the winner of a round.
+// Your game is going to play against the computer, so begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+
+// Write a function that plays a single round of Rock Paper Scissors.
+// The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
@@ -28,10 +27,27 @@ function playRound(playerSelection, computerSelection) {
     return "You Win! Scissors beats Paper";
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
     return "You lose! Rock beats Scissors";
+  } else if (
+    playerSelection != "scissors" ||
+    playerSelection != "rock" ||
+    playerSelection != "scissors"
+  ) {
+    return "Please type rock, paper, or scissors.";
   } else return "Draw!";
 }
 
-// Create a function called game that calls the playRound function inside of it to play a 5 round game that keeps score and reports a winnor loser at the end.
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
 
-function game() {}
+// Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = window.prompt("Paper, scissors, or rock?");
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
 console.log(game());
